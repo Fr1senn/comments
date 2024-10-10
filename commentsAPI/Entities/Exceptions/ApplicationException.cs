fb6770115b039ad9f0b0d4c27cca013c46f55614
Vlad Bomba <bombavlad1@gmail.com>
2024-10-10 11:19:01 +0300

    Custom exception implemented

---
+++ commentsAPI/Entities/Exceptions/ApplicationException.cs
@@ -0,0 +1,11 @@
+﻿namespace commentsAPI.Entities.Exceptions
+{
+    public class ApplicationException : Exception
+    {
+        public ApplicationException(string message)
+            : base(message) { }
+
+        public ApplicationException(string message, Exception innerException)
+          : base(message, innerException) { }
+    }
+}
