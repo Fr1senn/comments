25fd774809ab14e2422c5edce90b2537281a71ba
Vlad Bomba <bombavlad1@gmail.com>
2024-10-10 11:21:19 +0300

    Implemented middleware to catch exceptions

---
+++ commentsAPI/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using commentsAPI.Entities.Shared;
+using System.Net;
+using System.Text.Json;
+
+namespace commentsAPI.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var response = new ApiResponse
+            {
+                IsSuccess = false,
+                Message = ex.Message,
+                StatusCode = HttpStatusCode.InternalServerError
+            };
+
+            if (ex is ApplicationException)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
